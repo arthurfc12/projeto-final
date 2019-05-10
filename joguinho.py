6125da3b209d6364decdcd1d04883a29903edbef
unknown <arthurfc2@al.insper.edu.br>
2019-05-10 08:28:06 -0300

    5

--- joguinho.py
+++ joguinho.py
@@ -15,6 +15,10 @@ BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
 
+#Gravidade
+GRAV = -5
+
+
 class Player1(pygame.sprite.Sprite):
     
     
@@ -39,6 +43,12 @@ class Player1(pygame.sprite.Sprite):
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
         
+        # Velocidade da nave
+        self.speedx = 0
+        
+        # Melhora a colisão estabelecendo um raio de um circulo
+        self.radius = 25
+        
         def update(self):
             self.rect.x += self.speedx
             if self.rect.right > WIDTH:
@@ -46,11 +56,73 @@ class Player1(pygame.sprite.Sprite):
             if self.rect.left < 0:
                 self.rect.left = 0
                 
+            self.speedy -= GRAV
+            self.recty += self.speedy
             
+
+class Player2(pygame.sprite.Sprite):
+    
+    
+    def __init__(self, player_img2):
+        
+         # Construtor da classe pai (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+        
+        # Carregando a imagem de fundo.
+        self.image = player_img2
+        
+        # Diminuindo o tamanho da imagem.
+        self.image = pygame.transform.scale(player_img2, (50, 38))
+        
+        # Deixando transparente.
+        self.image.set_colorkey(BLACK)
+        
+        # Detalhes sobre o posicionamento.
+        self.rect = self.image.get_rect()
+        
+        # Centraliza embaixo da tela.
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        
+        # Velocidade da nave
+        self.speedx = 0
+        
+        # Melhora a colisão estabelecendo um raio de um circulo
+        self.radius = 25
+        
+        def update(self):
+            self.rect.x += self.speedx
+            if self.rect.right > WIDTH:
+                self.rect.right = WIDTH
+            if self.rect.left < 0:
+                self.rect.left = 0
+                
+            self.speedy -= GRAV
+            self.recty += self.speedy
+        
+
+                
+
+
+def game_screen(screen):            
         
         
         
         
+# Inicialização do Pygame.
+
+pygame.init()
+pygame.mixer.init()
+
+# Tamanho da tela.
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+# Comando para evitar travamentos.
+try:
+    game_screen(screen)
+finally:
+    pygame.quit()
         
         
         
