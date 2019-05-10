448bd4ee252740eb2b2679c8ef91e3839623835d
unknown <arthurfc2@al.insper.edu.br>
2019-05-09 21:07:08 -0300

    2

--- joguinho.py
+++ joguinho.py
@@ -1,1 +1,40 @@
 import pygame
+import time
+from os import path
+
+# Dados gerais do jogo.
+WIDTH = 480 # Largura da tela
+HEIGHT = 600 # Altura da tela
+FPS = 60 # Frames por segundo
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+
+class Player1(pygame.sprite.Sprite):
+    
+    
+    def __init__(self):
+        
+         # Construtor da classe pai (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+        
+        # Carregando a imagem de fundo.
+        self.image = player_img
+        
+        # Diminuindo o tamanho da imagem.
+        self.image = pygame.transform.scale(player_img, (50, 38))
+        
+        # Deixando transparente.
+        self.image.set_colorkey(BLACK)
+        
+        # Detalhes sobre o posicionamento.
+        self.rect = self.image.get_rect()
+        
+        # Centraliza embaixo da tela.
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
