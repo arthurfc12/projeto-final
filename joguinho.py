7d4e2af22258c062b6ed75e03fd5acf536d0089e
unknown <arthurfc2@al.insper.edu.br>
2019-05-10 11:02:26 -0300

    dasfgh

--- joguinho.py
+++ joguinho.py
@@ -2,11 +2,16 @@ import pygame
 import time
 from os import path
 
+# Estabelece a pasta que contem as figuras e sons.
+img_dir = path.join(path.dirname(__file__), 'img')
+snd_dir = path.join(path.dirname(__file__), 'snd')
+
 # Dados gerais do jogo.
 WIDTH = 480 # Largura da tela
 HEIGHT = 600 # Altura da tela
 FPS = 60 # Frames por segundo
 
+#Cores
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
