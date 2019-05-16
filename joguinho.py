b8a1bb31f82a699ee6d4f10dae1b55a8cd86a81f
SalaciousCrumb17 <lucasohara17@gmail.com>
2019-05-16 16:07:34 -0300

    garbage

--- joguinho.py
+++ joguinho.py
@@ -1,1 +1,20 @@
 import pygame
+from os import path
+
+
+WIDTH = 480 # Largura da tela
+HEIGHT = 600 # Altura da tela
+FPS = 60 # Frames por segundo
+
+
+
+
+class Player1():
+    
+
+
+    
+    
+    
+class player2():
+    
